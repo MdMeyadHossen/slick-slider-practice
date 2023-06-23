3f98293f878d1c80b67d78b38b8d589fa2c6fe63
Md Meyad Hossen <122221406+MdMeyadHossen@users.noreply.github.com>
2023-06-23 15:26:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// @Author=Md Meyad Hossen
+$(document).ready(function() {
+  $(".wrapper").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 4,
+    slidesToScroll: 2
+  });
+  $(".wrapper2").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  $(".wrapper3").slick({
+    lazyLoad: 'ondemand', // ondemand progressive anticipated
+    infinite: true
+  });
+  });
+
